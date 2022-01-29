5f8565ffb201189ff28a870ee7d9e3e9793c67df
Aleksandr_Saponchik <alexsapon@gmail.com>
2022-01-29 22:39:56 +0300

    fcc_29_1

--- index.js
+++ index.js
@@ -2,3 +2,6 @@
 
 let myStr = "This is the first sentence. ";
 myStr += "This is the second sentence.";
+// ----------------------------------------
+const myName = "Alex";
+const myStr = "Hello, my name is " + myName + ", how are you?";
