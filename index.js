8820133b018d05cf35816e228c561a35360be92d
Aleksandr_Saponchik <alexsapon@gmail.com>
2022-01-30 19:35:39 +0300

    30_01

--- index.js
+++ index.js
@@ -12,5 +12,7 @@ const myStr2 = "Hello, my name is " + myName + ", how are you?";
 // codewars
 
 const digitize = (n) => Array.from(String(n), Number).reverse();
-
 console.log(digitize(348597));
+// ---------------------------------------
+disemvowel = str => str.replace(/[aeuoi]/ig, '');
+console.log(disemvowel("This website is for losers LOL!"));
