3b7a5c5648d548f4266fba7ae2fcd08822d2158d
Aleksandr_Saponchik <alexsapon@gmail.com>
2022-02-03 21:27:01 +0300

    03_00

--- index.js
+++ index.js
@@ -176,3 +176,10 @@ const abbrevName = name => {
 
 console.log(abbrevName("Sam Harris"))
 // -------------------------------
+const feast = (beast, dish) => beast[0] === dish[0] && beast[beast.length - 1] === dish[dish.length - 1]
+
+console.log(feast("great blue heron", "garlic naan"))
+console.log(feast("chickadee", "chocolate cake"))
+console.log(feast("brown bear", "bear claw"))
+// --------------------------------
+const booleanToString = b => String(b);
